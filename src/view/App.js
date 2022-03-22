ebbda754c66beac1d9499903f732b99e65fa7a0f
Azimboy <azimboy1272@gmail.com>
2022-03-22 21:44:47 +0500

    first commit

---
+++ src/view/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./pages/components/navbar/Navbar";
+
+import { Home } from "./pages/Home";
+import { Info } from "./pages/Info";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/info" element={<Info/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/view/pages/components/todo/TodoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const TodoForm = () => {
+  return (
+    <form>
+            <div className="form-control">
+    <div className="input-group">
+        <input type="text" placeholder="Add your tasks here..." className="input input-bordered w-full input-lg"/>
+        <button className="btn btn-square btn-lg">+</button>
+    </div>
+    </div>
+    </form>
+  )
+}
