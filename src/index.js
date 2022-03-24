b58f5f0cb083e806f6e94a6f5202e6f38cc08bc3
Azimboy <azimboy1272@gmail.com>
2022-03-24 21:50:57 +0500

    second commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './view/index.css';
 import App from './view/App';
 import { BrowserRouter } from 'react-router-dom';
+import StoreContext from './logic/store'
 
 ReactDOM.render(
 <BrowserRouter>
+  <StoreContext >  
   <App />
+  </StoreContext>
 </BrowserRouter>,
   document.getElementById('root')
 );
